6235171ce1e7d6799d450296cee75773d17e27d2
mmarszal7 <mmarszal7@gmail.com>
2019-11-17 10:20:27 +0100

    Init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code!", "and the C++ extension!"};
+    cout << "Hello" << endl;
+    return 0;
+}
