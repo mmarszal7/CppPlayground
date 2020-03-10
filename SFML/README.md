f494cc4f295f9c25b763ae2cce54ad3ed12277dc
mmarszal7 <mmarszal7@gmail.com>
2020-03-10 16:22:36 +0100

    Add SFML sample

---
+++ SFML/README.md
@@ -0,0 +1,7 @@
+https://github.com/Hopson97/open-builder/blob/master/docs/building/Building_windows_vs.md#step-5-set-up-c17
+https://www.sfml-dev.org/tutorials/2.5/start-vc.php
+
+1. Set up C++ version
+2. C++ > Headers (/glm & /sfml/include)
+3. Linker > Libs (/sfml/lib)
+4. Linker input > bins (sfml-graphics.lib;sfml-system.lib;sfml-audio.lib;sfml-window.lib;sfml-network.lib)
---
+++ SFML/main.cpp
@@ -0,0 +1,82 @@
+#include <SFML/Graphics.hpp>
+
+int draw(int ifLower, int ifGreater)
+{
+	return rand() < RAND_MAX / 2 ? ifLower : ifGreater;
+}
+
+void update(sf::Shape& shape)
+{
+	shape.setRotation(
+		shape.getRotation() + draw(-20, 20)
+	);
+
+	shape.setPosition(
+		shape.getPosition().x + draw(-20, 20),
+		shape.getPosition().y + draw(-20, 20)
+	);
+
+	shape.setScale(
+		sf::Vector2f(draw(0.9, 1.1), draw(0.9, 1.1))
+	);
+
+	shape.setOutlineThickness(
+		shape.getOutlineThickness() + draw(-1, 1)
+	);
+
+	shape.setOutlineColor(sf::Color(
+		shape.getOutlineColor().r + draw(-1, 1),
+		shape.getOutlineColor().g + draw(-1, 1),
+		shape.getOutlineColor().b + draw(-1, 1))
+	);
+	shape.setFillColor(sf::Color(
+		shape.getFillColor().r + draw(-1, 1),
+		shape.getFillColor().g + draw(-1, 1),
+		shape.getFillColor().b + draw(-1, 1))
+	);
+}
+
+int main()
+{
+	srand(time(0));
+
+	// Shapes
+	//sf::CircleShape shape(80.f);
+	sf::RectangleShape shape(sf::Vector2f(80, 80));
+
+	// Color
+	shape.setFillColor(sf::Color::Green);
+	shape.setOutlineThickness(5.f);
+	shape.setOutlineColor(sf::Color::Red);
+
+	// Position
+	shape.setPosition(400, 400);
+
+	// Textures
+	sf::Texture texture;
+	texture.loadFromFile("texture.png");
+	shape.setTexture(&texture);
+
+	// Antialiasing
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	// Window
+	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML", sf::Style::Default, settings);
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+		update(shape);
+		sf::sleep(sf::milliseconds(100));
+	}
+
+	return 0;
+}
